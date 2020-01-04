79ad16d7a6e001a802bf2b3be44a4952356c81ea
gonggaobushang <47070965+gonggaobushang@users.noreply.github.com>
2020-01-04 21:02:26 +0800

    Update function def.py

--- function def.py
+++ function def.py
@@ -37,3 +37,28 @@ sorted([-12,32,2],key=abs)
 #对字符串排序，是按照ASCII的大小比较的，Z' < 'a'
 sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower,reverse=True)
 
+#闭包
+def count():
+    def f(j):
+        def g():
+            return j*j
+        return g #返回的是这个函数，在必要的时候再进行计算
+    fs = []
+    for i in range(1, 4):
+        print(f(i))
+        fs.append(f(i)) # f(i)立刻被执行，因此i的当前值被传入f()
+        print(fs)
+    return fs
+
+f1,f2,f3 = count()
+f2()
+f1()
+f3()
+
+#匿名函数
+#lambda表示匿名函数，冒号前面的x表示函数参数
+list(map(lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+f = lambda x:x*x
+f(4)
+#__name__函数的名字
+f.__name__
